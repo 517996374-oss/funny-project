1b45a87a1a345ffeef29536d90fd5803641af89b
517996374-oss <517996374-oss@users.noreply.github.com>
2026-07-01 13:30:05 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+def main():
+    print("Hello from funny-project!")
+
+
+if __name__ == "__main__":
+    main()
+
+git clone https://github.com/denzow/iott.git
+$ export SLACK_TOKEN=your_token 
+- python3 /path/to/bot.py
++ python3 /home/pi/apps/toi/bot.py
+[Service]
+- ExecStart = /path/to/wcpaccho.sh
++ ExecStart = /home/pi/apps/toi/wcpaccho.sh
+$ sudo cp toiletmonitor.service /etc/systemd/system/
+
+
